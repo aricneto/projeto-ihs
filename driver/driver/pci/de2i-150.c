119e438b0e64d05262bd1856006b2bbbe08996b1
Ariovaldo Neto <a.neto@strategiesnorth.ca>
2023-09-16 00:32:12 -0300

    tabs to spaces

--- driver/driver/pci/de2i-150.c
+++ driver/driver/pci/de2i-150.c
@@ -1,12 +1,12 @@
 #include <linux/init.h>
-#include <linux/module.h>	/* THIS_MODULE macro */
-#include <linux/fs.h>		/* VFS related */
+#include <linux/module.h>    /* THIS_MODULE macro */
+#include <linux/fs.h>        /* VFS related */
 #include <linux/ioctl.h>    /* ioctl syscall */
-#include <linux/errno.h>	/* error codes */
-#include <linux/types.h>	/* dev_t number */
-#include <linux/cdev.h>		/* char device registration */
-#include <linux/uaccess.h>	/* copy_*_user functions */
-#include <linux/pci.h>		/* pci funcs and types */
+#include <linux/errno.h>    /* error codes */
+#include <linux/types.h>    /* dev_t number */
+#include <linux/cdev.h>        /* char device registration */
+#include <linux/uaccess.h>    /* copy_*_user functions */
+#include <linux/pci.h>        /* pci funcs and types */
 
 #include "../../include/ioctl_cmds.h"
 
@@ -32,11 +32,11 @@ static void __exit my_exit (void);
 
 /* char device system calls */
 
-static int	my_open   (struct inode*, struct file*);
-static int 	my_close  (struct inode*, struct file*);
-static ssize_t 	my_read   (struct file*, char __user*, size_t, loff_t*);
-static ssize_t 	my_write  (struct file*, const char __user*, size_t, loff_t*);
-static long int	my_ioctl  (struct file*, unsigned int, unsigned long);
+static int         my_open   (struct inode*, struct file*);
+static int         my_close  (struct inode*, struct file*);
+static ssize_t     my_read   (struct file*, char __user*, size_t, loff_t*);
+static ssize_t     my_write  (struct file*, const char __user*, size_t, loff_t*);
+static long int    my_ioctl  (struct file*, unsigned int, unsigned long);
 
 /* pci functions */
 
@@ -46,29 +46,29 @@ static void __exit my_pci_remove (struct pci_dev *dev);
 /* pci ids which this driver supports */
 
 static struct pci_device_id pci_ids[] = {
-	{PCI_DEVICE(MY_PCI_VENDOR_ID, MY_PCI_DEVICE_ID), },
-	{0, }
+    {PCI_DEVICE(MY_PCI_VENDOR_ID, MY_PCI_DEVICE_ID), },
+    {0, }
 };
 MODULE_DEVICE_TABLE(pci, pci_ids);
 
 /* device file operations */
 
 static struct file_operations fops = {
-	.owner = THIS_MODULE,
-	.read = my_read,
-	.write = my_write,
-	.unlocked_ioctl	= my_ioctl,
-	.open = my_open,
-	.release = my_close
+    .owner = THIS_MODULE,
+    .read = my_read,
+    .write = my_write,
+    .unlocked_ioctl = my_ioctl,
+    .open = my_open,
+    .release = my_close
 };
 
 /* pci driver operations */
 
 static struct pci_driver pci_ops = {
-	.name = DRIVER_NAME,
-	.id_table = pci_ids,
-	.probe = my_pci_probe,
-	.remove = my_pci_remove
+    .name = DRIVER_NAME,
+    .id_table = pci_ids,
+    .probe = my_pci_probe,
+    .remove = my_pci_remove
 };
 
 /* variables for char device registration to kernel */
@@ -93,21 +93,21 @@ uint8_t *kernel_buffer_write;
 
 /* peripherals names for debugging in dmesg */
 static const char* peripheral[] = {
-	"switches",
-	"p_buttons",
-	"display_l",
-	"display_r",
-	"green_leds",
-	"red_leds"
+    "switches",
+    "p_buttons",
+    "display_l",
+    "display_r",
+    "green_leds",
+    "red_leds"
 };
 
 enum perf_names_idx {
-	IDX_SWITCH = 0,
-	IDX_PBUTTONS,
-	IDX_DISPLAYL,
-	IDX_DISPLAYR,
-	IDX_GREENLED,
-	IDX_REDLED
+    IDX_SWITCH = 0,
+    IDX_PBUTTONS,
+    IDX_DISPLAYL,
+    IDX_DISPLAYR,
+    IDX_GREENLED,
+    IDX_REDLED
 };
 static int wr_name_idx = IDX_DISPLAYR;
 static int rd_name_idx = IDX_SWITCH;
@@ -116,69 +116,69 @@ static int rd_name_idx = IDX_SWITCH;
 
 static int __init my_init(void)
 {
-	printk("my_driver: loaded to the kernel\n");
+    printk("my_driver: loaded to the kernel\n");
 
-	/* 0. register pci driver to the kernel */
-	if (pci_register_driver(&pci_ops) < 0) {
-		printk("my_driver: PCI driver registration failed\n");
-		return -EAGAIN;
-	}
+    /* 0. register pci driver to the kernel */
+    if (pci_register_driver(&pci_ops) < 0) {
+        printk("my_driver: PCI driver registration failed\n");
+        return -EAGAIN;
+    }
 
-	/* 1. request the kernel for a device number */
-	if (alloc_chrdev_region(&my_device_nbr, 0, 1, DRIVER_NAME) < 0) {
-		printk("my_driver: device number could not be allocated!\n");
-		return -EAGAIN;
-	}
-	printk("my_driver: device number %d was registered!\n", MAJOR(my_device_nbr));
+    /* 1. request the kernel for a device number */
+    if (alloc_chrdev_region(&my_device_nbr, 0, 1, DRIVER_NAME) < 0) {
+        printk("my_driver: device number could not be allocated!\n");
+        return -EAGAIN;
+    }
+    printk("my_driver: device number %d was registered!\n", MAJOR(my_device_nbr));
 
-	/* 2. create class : appears at /sys/class */
-	if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
-		printk("my_driver: device class count not be created!\n");
-		goto ClassError;
-	}
+    /* 2. create class : appears at /sys/class */
+    if ((my_class = class_create(THIS_MODULE, DRIVER_CLASS)) == NULL) {
+        printk("my_driver: device class count not be created!\n");
+        goto ClassError;
+    }
 
-	/* 3. associate the cdev with a set of file operations */
-	cdev_init(&my_device, &fops);
+    /* 3. associate the cdev with a set of file operations */
+    cdev_init(&my_device, &fops);
 
-	/* 4. create the device node */
-	if (device_create(my_class, NULL, my_device_nbr, NULL, FILE_NAME) == NULL) {
-		printk("my_driver: can not create device file!\n");
-		goto FileError;
-	}
+    /* 4. create the device node */
+    if (device_create(my_class, NULL, my_device_nbr, NULL, FILE_NAME) == NULL) {
+        printk("my_driver: can not create device file!\n");
+        goto FileError;
+    }
 
-	/* 5. now make the device live for the users to access */
-	if (cdev_add(&my_device, my_device_nbr, 1) == -1){
-		printk("my_driver: registering of device to kernel failed!\n");
-		goto AddError;
-	}
+    /* 5. now make the device live for the users to access */
+    if (cdev_add(&my_device, my_device_nbr, 1) == -1){
+        printk("my_driver: registering of device to kernel failed!\n");
+        goto AddError;
+    }
 
-	return 0;
+    return 0;
 
 AddError:
-	device_destroy(my_class, my_device_nbr);
+    device_destroy(my_class, my_device_nbr);
 FileError:
-	class_destroy(my_class);
+    class_destroy(my_class);
 ClassError:
-	unregister_chrdev(my_device_nbr, DRIVER_NAME);
-	pci_unregister_driver(&pci_ops);
-	return -EAGAIN;
+    unregister_chrdev(my_device_nbr, DRIVER_NAME);
+    pci_unregister_driver(&pci_ops);
+    return -EAGAIN;
 }
 
 static void __exit my_exit(void)
 {
-	cdev_del(&my_device);
-	device_destroy(my_class, my_device_nbr);
-	class_destroy(my_class);
-	unregister_chrdev(my_device_nbr, DRIVER_NAME);
-	pci_unregister_driver(&pci_ops);
-	printk("my_driver: goodbye kernel!\n");
+    cdev_del(&my_device);
+    device_destroy(my_class, my_device_nbr);
+    class_destroy(my_class);
+    unregister_chrdev(my_device_nbr, DRIVER_NAME);
+    pci_unregister_driver(&pci_ops);
+    printk("my_driver: goodbye kernel!\n");
 }
 
 static int my_open(struct inode* inode, struct file* filp)
 {
-	/*Creating Physical memory*/
+    /*Creating Physical memory*/
     if((kernel_buffer_read = kmalloc(mem_size , GFP_KERNEL)) == 0 
-		|| (kernel_buffer_write = kmalloc(mem_size , GFP_KERNEL)) == 0 ){
+        || (kernel_buffer_write = kmalloc(mem_size , GFP_KERNEL)) == 0 ){
         printk(KERN_INFO "my_driver: cannot allocate memory in kernel\n");
         return -1;
     }
@@ -188,151 +188,151 @@ static int my_open(struct inode* inode, struct file* filp)
 
 static int my_close(struct inode* inode, struct file* filp)
 {
-	// Liberando espaço do buffer
-	kfree(kernel_buffer_read);
-	kfree(kernel_buffer_write);
-	printk("my_driver: close was called\n");
-	return 0;
+    // Liberando espaço do buffer
+    kfree(kernel_buffer_read);
+    kfree(kernel_buffer_write);
+    printk("my_driver: close was called\n");
+    return 0;
 }
 
 static ssize_t my_read(struct file* filp, char __user* buf, size_t count, loff_t* f_pos)
 {
-	ssize_t retval = 0;
-	int to_cpy = 0;
+    ssize_t retval = 0;
+    int to_cpy = 0;
 
-	/* check if the read_pointer pointer is set */
-	if (read_pointer == NULL) {
-		printk("my_driver: trying to read to a device region not set yet\n");
-		return -ECANCELED;
-	}
+    /* check if the read_pointer pointer is set */
+    if (read_pointer == NULL) {
+        printk("my_driver: trying to read to a device region not set yet\n");
+        return -ECANCELED;
+    }
 
-	/* read from the device */
-	kernel_buffer_read = ioread32(read_pointer);
-	printk("my_driver: red 0x%X from the %s\n", kernel_buffer_read, peripheral[rd_name_idx]);
+    /* read from the device */
+    kernel_buffer_read = ioread32(read_pointer);
+    printk("my_driver: red 0x%X from the %s\n", kernel_buffer_read, peripheral[rd_name_idx]);
 
-	/* get amount of bytes to copy to user */
-	to_cpy = (count <= sizeof(kernel_buffer_read)) ? count : sizeof(kernel_buffer_read);
+    /* get amount of bytes to copy to user */
+    to_cpy = (count <= sizeof(kernel_buffer_read)) ? count : sizeof(kernel_buffer_read);
 
-	/* copy data to user */
-	retval = to_cpy - copy_to_user(buf, &kernel_buffer_read, to_cpy);
+    /* copy data to user */
+    retval = to_cpy - copy_to_user(buf, &kernel_buffer_read, to_cpy);
 
-	return retval;
+    return retval;
 }
 
 static ssize_t my_write(struct file* filp, const char __user* buf, size_t count, loff_t* f_pos)
 {
-	ssize_t retval = 0;
-	int to_cpy = 0;
+    ssize_t retval = 0;
+    int to_cpy = 0;
 
-	/* check if the write_pointer pointer is set */
-	if (write_pointer == NULL) {
-		printk("my_driver: trying to write to a device region not set yet\n");
-		return -ECANCELED;
-	}
+    /* check if the write_pointer pointer is set */
+    if (write_pointer == NULL) {
+        printk("my_driver: trying to write to a device region not set yet\n");
+        return -ECANCELED;
+    }
 
-	/* get amount of bytes to copy from user */
-	to_cpy = (count <= sizeof(kernel_buffer_write)) ? count : sizeof(kernel_buffer_write);
+    /* get amount of bytes to copy from user */
+    to_cpy = (count <= sizeof(kernel_buffer_write)) ? count : sizeof(kernel_buffer_write);
 
-	/* copy data from user */
-	retval = to_cpy - copy_from_user(&kernel_buffer_write, buf, to_cpy);
+    /* copy data from user */
+    retval = to_cpy - copy_from_user(&kernel_buffer_write, buf, to_cpy);
 
-	/* send to device */
-	iowrite32(kernel_buffer_write, write_pointer);
-	printk("my_writer: wrote 0x%X to the %s\n", kernel_buffer_write, peripheral[wr_name_idx]);
+    /* send to device */
+    iowrite32(kernel_buffer_write, write_pointer);
+    printk("my_writer: wrote 0x%X to the %s\n", kernel_buffer_write, peripheral[wr_name_idx]);
 
-	return retval;
+    return retval;
 }
 
 static long int my_ioctl(struct file*, unsigned int cmd, unsigned long arg)
 {
-	switch(cmd){
-	case RD_SWITCHES:
-		read_pointer = bar0_mmio + 0xF080;
-		rd_name_idx = IDX_SWITCH;
-		break;
-	case RD_PBUTTONS:
-		read_pointer = bar0_mmio + 0xF0A0;
-		rd_name_idx = IDX_PBUTTONS;
-		break;
-	case WR_L_DISPLAY:
-		write_pointer = bar0_mmio + 0xF020;
-		wr_name_idx = IDX_DISPLAYL;
-		break;
-	case WR_R_DISPLAY:
-		write_pointer = bar0_mmio + 0xFA00;
-		wr_name_idx = IDX_DISPLAYR;
-		break;
-	case WR_RED_LEDS:
-		write_pointer = bar0_mmio + 0xFB40;
-		wr_name_idx = IDX_REDLED;
-		break;
-	case WR_GREEN_LEDS:
-		write_pointer = bar0_mmio + 0xF060;
-		wr_name_idx = IDX_GREENLED;
-		break;
-	default:
-		printk("my_driver: unknown ioctl command: 0x%X\n", cmd);
-	}
-	return 0;
+    switch(cmd){
+    case RD_SWITCHES:
+        read_pointer = bar0_mmio + 0xF080;
+        rd_name_idx = IDX_SWITCH;
+        break;
+    case RD_PBUTTONS:
+        read_pointer = bar0_mmio + 0xF0A0;
+        rd_name_idx = IDX_PBUTTONS;
+        break;
+    case WR_L_DISPLAY:
+        write_pointer = bar0_mmio + 0xF020;
+        wr_name_idx = IDX_DISPLAYL;
+        break;
+    case WR_R_DISPLAY:
+        write_pointer = bar0_mmio + 0xFA00;
+        wr_name_idx = IDX_DISPLAYR;
+        break;
+    case WR_RED_LEDS:
+        write_pointer = bar0_mmio + 0xFB40;
+        wr_name_idx = IDX_REDLED;
+        break;
+    case WR_GREEN_LEDS:
+        write_pointer = bar0_mmio + 0xF060;
+        wr_name_idx = IDX_GREENLED;
+        break;
+    default:
+        printk("my_driver: unknown ioctl command: 0x%X\n", cmd);
+    }
+    return 0;
 }
 
 static int __init my_pci_probe(struct pci_dev *dev, const struct pci_device_id *id)
 {
-	unsigned short vendor, device;
-	unsigned char rev;
-	unsigned int bar_value;
-	unsigned long bar_len;
+    unsigned short vendor, device;
+    unsigned char rev;
+    unsigned int bar_value;
+    unsigned long bar_len;
 
-	/* enable the device */
-	if (pci_enable_device(dev) < 0) {
-		printk("my_driver: Could not enable the PCI device!\n");
-		return -EBUSY;
-	}
-	printk("my_driver: PCI device enabled\n");
+    /* enable the device */
+    if (pci_enable_device(dev) < 0) {
+        printk("my_driver: Could not enable the PCI device!\n");
+        return -EBUSY;
+    }
+    printk("my_driver: PCI device enabled\n");
 
-	/* read some info from the PCI configuration space */
-	pci_read_config_word(dev, PCI_VENDOR_ID, &vendor);
-	pci_read_config_word(dev, PCI_DEVICE_ID, &device);
-	pci_read_config_byte(dev, PCI_REVISION_ID, &rev);
-	printk("my_driver: PCI device - Vendor 0x%X Device 0x%X Rev 0x%X\n", vendor, device, rev);
+    /* read some info from the PCI configuration space */
+    pci_read_config_word(dev, PCI_VENDOR_ID, &vendor);
+    pci_read_config_word(dev, PCI_DEVICE_ID, &device);
+    pci_read_config_byte(dev, PCI_REVISION_ID, &rev);
+    printk("my_driver: PCI device - Vendor 0x%X Device 0x%X Rev 0x%X\n", vendor, device, rev);
 
-	/* read info about the PCI device BAR0 */
-	pci_read_config_dword(dev, 0x10, &bar_value);
-	bar_len = pci_resource_len(dev, 0);
-	printk("my_driver: PCI device - BAR0 => 0x%X with length of %ld Kbytes\n", bar_value, bar_len/1024);
+    /* read info about the PCI device BAR0 */
+    pci_read_config_dword(dev, 0x10, &bar_value);
+    bar_len = pci_resource_len(dev, 0);
+    printk("my_driver: PCI device - BAR0 => 0x%X with length of %ld Kbytes\n", bar_value, bar_len/1024);
 
-	/* mark the PCI BAR0 region as reserved to this driver */
-	if (pci_request_region(dev, 0, DRIVER_NAME) != 0) {
-		printk("my_driver: PCI Error - PCI BAR0 region already in use!\n");
-		pci_disable_device(dev);
-		return -EBUSY;
-	}
+    /* mark the PCI BAR0 region as reserved to this driver */
+    if (pci_request_region(dev, 0, DRIVER_NAME) != 0) {
+        printk("my_driver: PCI Error - PCI BAR0 region already in use!\n");
+        pci_disable_device(dev);
+        return -EBUSY;
+    }
 
-	/* map the BAR0 Physical address space to virtual space */
-	bar0_mmio = pci_iomap(dev, 0, bar_len);
+    /* map the BAR0 Physical address space to virtual space */
+    bar0_mmio = pci_iomap(dev, 0, bar_len);
 
-	/* initialize a default peripheral read and write pointer */
-	write_pointer = bar0_mmio + 0xF040; //TODO: update offset
-	read_pointer  = bar0_mmio + 0xF080; //TODO: update offset
+    /* initialize a default peripheral read and write pointer */
+    write_pointer = bar0_mmio + 0xF040; //TODO: update offset
+    read_pointer  = bar0_mmio + 0xF080; //TODO: update offset
 
-	return 0;
+    return 0;
 }
 
 static void __exit my_pci_remove(struct pci_dev *dev)
 {
-	read_pointer = NULL;
-	write_pointer = NULL;
+    read_pointer = NULL;
+    write_pointer = NULL;
 
-	/* remove the IO mapping done in probe func */
-	pci_iounmap(dev, bar0_mmio);
+    /* remove the IO mapping done in probe func */
+    pci_iounmap(dev, bar0_mmio);
 
-	/* disable the PCI device */
-	pci_disable_device(dev);
+    /* disable the PCI device */
+    pci_disable_device(dev);
 
-	/* unmark device PCI BAR0 as reserved */
-	pci_release_region(dev, 0);
+    /* unmark device PCI BAR0 as reserved */
+    pci_release_region(dev, 0);
 
-	printk("my_driver: PCI Device - Disabled and BAR0 Released");
+    printk("my_driver: PCI Device - Disabled and BAR0 Released");
 }
 
 module_init(my_init);
